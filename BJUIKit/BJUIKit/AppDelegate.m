d4feeb2d824aa94ede19a150eeb909260090a2f4
yqhbj <1073972444@qq.com>
2021-06-07 18:12:28 +0800

    123

--- BJUIKit/BJUIKit/AppDelegate.m
+++ BJUIKit/BJUIKit/AppDelegate.m
@@ -17,6 +17,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSString *str = @"";
+    12312312312312
     return YES;
 }
 
